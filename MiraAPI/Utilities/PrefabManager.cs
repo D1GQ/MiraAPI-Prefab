3b7d5d7e86ed066a3f06dc52e6a2a3240b377c93
D1GQ <yourboylax@gmail.com>
2025-02-09 21:23:43 -0600

    I didn't do that XD

--- MiraAPI/Utilities/PrefabManager.cs
+++ MiraAPI/Utilities/PrefabManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-namespace TheBetterRoles.AmongUsPrefabs;
+namespace MiraAPI.Utilities;
 
 // Original code from: https://github.com/D1GQ/AmongUsPrefabsAPI
 
